ecd83cc96d16cf2e596a2eeab0ef28a3697ab838
MSRbuddy <meghanadasari222@gmail.com>
2023-04-20 14:57:06 +0530

    Added : Linked List Program UC-5!

---
+++ LList-UC5/LList-UC5/LinkedList.cs
@@ -0,0 +1,109 @@
+﻿namespace LList_UC5
+{
+    public class LinkedList
+    {
+        public Node head;
+        // UC-1 Adding the node to the end of the linked list
+        public void InsertEnd(int newData)
+        {
+            Node newNode = new Node(newData);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = newNode;
+            }
+        }
+        public Node GetLastNode()
+        {
+            Node temp = this.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        public void DisplayList()
+        {
+            Node temp = this.head; 
+            if (temp == null)
+            {
+                Console.WriteLine("The Linked List is Empty...");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.value + "  ");
+                    temp = temp.next;
+                }
+            }
+        }
+        // UC2- Inserting the element at the head of the linked list
+        public void InsertInFront(int newData)
+        {
+            Node newNode = new Node(newData);
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        // UC-3 Appending the node to the end of the linked list
+        public void Append(int newData)
+        {
+            Node newNode = new Node(newData);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = newNode;
+            }
+        }
+        // UC4- Inserting the node at a particular position in the linked list
+        public Node InsertAtParticularPosition(int position, int newData)
+        {
+            if (position < 1)
+                Console.WriteLine("The Position passed is invalid to insert...");
+            else if (position == 1)
+            {
+                Node newNode = new Node(newData);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node temp = new Node(newData);
+                        temp.next = this.head.next;
+                        head.next = temp;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position Out Of Range....");
+            }
+            Console.WriteLine("Inserted Value is - " + head.next.value);
+            return head;
+        }
+        // UC5- Deleting the first node of the linked list
+        public Node DeleteFirstNode()
+        {
+            if (this.head == null)
+                return null;
+            else
+            {
+                this.head = this.head.next;
+                return this.head;
+            }
+        }
+    }
+}
---
+++ LList-UC5/LList-UC5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LList_UC5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Linked List Program UC-6*****\n");
+            
+            //Creating the instance of the Linked List Class
+            LinkedList newList = new LinkedList();
+            newList.Append(56);
+            newList.Append(70);
+            newList.InsertAtParticularPosition(2, 30);
+            newList.DeleteFirstNode();
+            newList.DisplayList();
+            Console.WriteLine();
+        }
+    }
+}
