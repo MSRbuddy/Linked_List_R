481a2c337e29491819f3faae1c79b9649abea5fa
MSRbuddy <meghanadasari222@gmail.com>
2023-04-20 14:59:28 +0530

    Added : Linked List Program UC-7!

---
+++ LList-UC7/LList-UC7/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace LList_UC7
+{
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ LList-UC7/LList-UC7/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LList_UC7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Linked List Program UC-7*****\n");
+                      
+            //Creating the instance of the Linked List Class
+            LinkedList newList = new LinkedList();
+            newList.Append(56);
+            newList.Append(70);
+            newList.InsertAtParticularPosition(2, 30);
+            newList.DisplayList();
+            Console.WriteLine("\nPostion of the element 30 is  " + newList.SearchTheNode(30));
+            Console.WriteLine();
+        }
+    }
+}
