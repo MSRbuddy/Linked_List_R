cd95af48a19078fe139b79852febfe7cf2e25427
MSRbuddy <meghanadasari222@gmail.com>
2023-04-20 15:00:57 +0530

    Added : Linked List Program UC-8!

---
+++ LList-UC8/LList-UC8/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LList_UC8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Linked List Program UC-8*****\n");
+            
+            //Creating the instance of the Linked List Class
+            LinkedList newList = new LinkedList();
+            newList.Append(56);
+            newList.Append(70);
+            newList.InsertAtParticularPosition(2, 30);
+            Console.WriteLine("\nLinked List Status......");
+            newList.DisplayList();
+            Console.WriteLine("\nPostion of the element 30 is  " + newList.SearchTheNode(30));
+            int position = newList.SearchTheNode(30);
+            newList.InsertAtParticularPosition(position + 1, 40);
+            Console.WriteLine("\nLinked List Status......");
+            newList.DisplayList();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LList-UC8/LimkedListTestCases/UnitTest1.cs
@@ -0,0 +1,26 @@
+using LList_UC8;
+
+namespace LinkedListTestCases
+{
+    [TestClass]
+    public class LinkedListTestCases
+    {
+        // Testing the Position of the node of the linked list
+        [DataRow(30, 2)]
+        [TestCategory("Testing the position of the node")]
+        [TestMethod]
+        public void TestForPositionInTheLinkedList(int value, int position)
+        {
+            //Arrange
+            LinkedList linkedList = new LinkedList();
+            linkedList.InsertInFront(56);
+            linkedList.InsertEnd(70);
+            linkedList.InsertAtParticularPosition(2, 30);
+            //Act
+            int expected = 2;
+            int actual = linkedList.SearchTheNode(30);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
