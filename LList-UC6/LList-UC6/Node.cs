0a9d8d147bc4cbb3c6c81dd21d6df075dbab833d
MSRbuddy <meghanadasari222@gmail.com>
2023-04-20 14:58:14 +0530

    Added : Linked List Program UC-6!

---
+++ LList-UC6/LList-UC6/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace LList_UC6
+{
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int value)
+        {
+            this.value = value;
+        }
+    }
+}
