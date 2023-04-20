1ac17a0bd15b57939da8fb4e57633f2df7f807a6
MSRbuddy <meghanadasari222@gmail.com>
2023-04-20 14:54:17 +0530

    Added : Linked List Program UC-3!

---
+++ LList-UC3/LList-UC3/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace LList_UC3
+{
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ LList-UC3/LList-UC3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LList_UC3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Linked List Program UC-3*****\n");
+            
+            //Creating the instance of the Linked List Class
+            LinkedList newList = new LinkedList();
+            newList.Append(56);
+            newList.Append(30);
+            newList.Append(70);
+            newList.DisplayList();
+            Console.WriteLine();
+        }
+    }
+}
