22facf479e3f9b71cff5bf7b448ec684ada195ea
MSRbuddy <meghanadasari222@gmail.com>
2023-04-20 14:49:39 +0530

    Added : Linked List Program UC-2!

---
+++ LList-UC2/LList-UC2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LList_UC2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Linked List Program UC-2*****\n");
+            
+            //Creating the instance of the Linked List Class
+            LinkedList newList = new LinkedList();
+            newList.InsertInFront(70);
+            newList.InsertInFront(30);
+            newList.InsertInFront(56);
+            newList.DisplayList();
+            Console.WriteLine();
+        }
+    }
+}
