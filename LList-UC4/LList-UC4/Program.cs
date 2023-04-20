41fbaf0cac587dd94ef7b38cf4481530546e367c
MSRbuddy <meghanadasari222@gmail.com>
2023-04-20 14:55:45 +0530

    Added : Linked List Program UC-4!

---
+++ LList-UC4/LList-UC4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LList_UC4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Linked List Program UC-4*****\n");
+            
+            //Creating the instance of the Linked List Class
+            LinkedList newList = new LinkedList();
+            newList.Append(56);
+            newList.Append(70);
+            newList.InsertAtParticularPosition(2, 30);
+            newList.DisplayList();
+            Console.WriteLine();
+        }
+    }
+}
